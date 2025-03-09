d9b445ae236a9edbdbe598575d8d04ee45a922de
Nitin Verma <nitinverma9784@gmail.com>
2025-03-09 22:29:22 +0530

    minor tweak

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,7 +16,7 @@ export default function About() {
       <Card className="bg-gradient-to-r from-purple-500 to-pink-500 shadow-lg text-white">
         <CardHeader>
           <CardTitle className="text-4xl font-bold text-center">
-            About DSA Visualizer 🚀
+            About DSA Visualizer 
           </CardTitle>
         </CardHeader>
         <CardContent>
