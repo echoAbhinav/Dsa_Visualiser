8d8bfda44dbe2847f3a44e336b739e55f575f2fb
Nitin Verma <nitinverma9784@gmail.com>
2025-03-17 21:55:45 +0530

    add data structure section on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 
 import Hero from '../Hero'
+import DataStructureCategories from '@/data-structure-categories';
 import AlgorithmCategories from '../AlgorithmCategories';
 function Home() {
   return (
     <div className='dark:bg-black'>
       <Hero />
       <AlgorithmCategories/>
-    
+
+    <DataStructureCategories/>
     </div>
   )
 }
